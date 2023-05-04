131417ddb913c6b064ef02cb0a047b870d2bcb49
Shodhan <24584877+shodhangk@users.noreply.github.com>
2023-05-05 00:20:45 +0530

    .sql

---
+++ top_entries.sql
@@ -0,0 +1,15 @@
+/*To retrieve two minimum salaries, you can write a query as below:*/
+SELECT DISTINCT Salary FROM EmployeePosition E1 
+ WHERE 2 >= (SELECT COUNT(DISTINCT Salary)FROM EmployeePosition E2 
+  WHERE E1.Salary >= E2.Salary) ORDER BY E1.Salary DESC;
+
+/*To retrieve two maximum salaries, you can write a query as below: */
+
+SELECT DISTINCT Salary FROM EmployeePosition E1 
+ WHERE 2 >= (SELECT COUNT(DISTINCT Salary) FROM EmployeePosition E2 
+  WHERE E1.Salary <= E2.Salary) ORDER BY E1.Salary DESC;
+  
+  
+/*retrieve Departments who have less than 2 employees working in it.*/
+
+SELECT DEPARTMENT, COUNT(EmpID) as 'EmpNo' FROM EmployeeInfo GROUP BY DEPARTMENT HAVING COUNT(EmpD) < 2;
